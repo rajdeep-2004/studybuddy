077f5a65b8c6311bf2586d4815ac3e36096e3338
Rajdeep <rajdeepsanyal24@gmail.com>
2025-05-07 16:52:36 +0530

    Landing Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar.jsx"
+import Banner from "./components/Banner/Banner.jsx"
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Banner/>
     </>
   )
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Banner.css"
+
+const Banner = () => {
+  return (
+        <div className='content'>
+            <h1>Study Smarter, Together</h1>
+            <p>Form study groups, share resources, and ace your exams with Study Buddy - the ultimate collaborative learning platform.</p>
+            <button className="cta">Get Started</button>
+        </div>
+
+  )
+}
+
+export default Banner
