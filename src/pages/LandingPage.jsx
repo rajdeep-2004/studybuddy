cd946674405fb764a7cefa798e5112e175b7dcab
Rajdeep <rajdeepsanyal24@gmail.com>
2025-07-06 00:41:48 +0530

    Navbar Component and Landing Page Made.

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,25 @@
+import Navbar from "../components/Navbar/Navbar.jsx"
+
+function LandingPage (){
+    return (
+        <>
+            <Navbar/>
+
+            {/* Banner and CTA */}
+
+            <section className='Banner'>
+            <div className="flex justify-between">
+                <div className="flex flex-col justify-center">
+                    <h2 className='text-9xl ml-10'>Study. Smarter. Together.</h2>
+                    <button className="px-4 py-2 rounded-lg text-white font-semibold w-50 ml-15 mt-20 bg-[rgb(109,191,254)] border-2 border-[rgb(109,191,254)] hover:bg-white hover:text-black transition">Get Started</button>
+
+                </div>
+
+                <img className="h-220 w-250 mr-0" src="src/assets/StudyGroup.jpg" alt="Banner" />
+            </div>
+        </section>
+        </>
+    )
+}
+
+export default LandingPage 
