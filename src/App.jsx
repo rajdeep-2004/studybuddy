b126e7adeb90faa98c5c9e9a305d2c351b500fa1
Rajdeep <rajdeepsanyal24@gmail.com>
2025-11-12 18:31:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignUp from "./pages/SignUpPage";
 import Login from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage"
+import Features from "./pages/Features"
 
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/" element={<LandingPage/>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/features" element={<Features />} />
         <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
       </Routes>
     </Router>
