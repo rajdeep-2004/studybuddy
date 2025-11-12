b04d9b84dea376c586664a592d4ee4e49b065a9b
Rajdeep <rajdeepsanyal24@gmail.com>
2025-11-12 18:03:59 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { UserDataProvider } from "./context/UserDataContext.jsx";
+// import { AuthProvider } from "./context/AuthContext.jsx";
+// import { UserDataProvider } from "./context/UserDataContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <UserDataProvider>
+    {/* <AuthProvider> */}
+      {/* <UserDataProvider> */}
         <App />
-      </UserDataProvider>
-    </AuthProvider>
+      {/* </UserDataProvider> */}
+    {/* </AuthProvider> */}
   </StrictMode>
 );
